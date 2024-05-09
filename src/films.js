3da40695a8e7dd6863e04207f57798c9ced26b04
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-05-09 11:10:24 +0200

    Spring 3 - Testing

--- src/films.js
+++ src/films.js
@@ -7,8 +7,7 @@ function getAllDirectors(movies) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(movies, director) {
-  let result2 = movies.filter(item => item.director === director);
-  if (director === 'Quentin Tarantino') {
+  let result2 = movies.filter(item => item.director === director); {
     console.log("EXERCICE 2 ->", result2);
   }
   return result2;
@@ -18,8 +17,7 @@ function getMoviesFromDirector(movies, director) {
 function moviesAverageOfDirector(movies, director) {
 
   let moviesOfDirector = movies.filter(movie => movie.director === director);
-  if (director === 'Quentin Tarantino'); 
-
+   
   let puntuacionTotal = moviesOfDirector.reduce((total, movie) => total + movie.score, 0);
 
   let promedioPuntuacion = puntuacionTotal / moviesOfDirector.length;
@@ -31,32 +29,107 @@ function moviesAverageOfDirector(movies, director) {
 }
 
 
-
-
 // Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
+function orderAlphabetically(movies) {
+  let movieTitles = movies.map(movie => movie.title);
+
+  // Ordenar alfabéticamente los títulos
+    let sortedTitles = movieTitles.sort((a, b) => {
+      let titleA = a.toLowerCase();
+      let titleB = b.toLowerCase();
   
-}
+      if (titleA < titleB) {
+        return -1;
+      }
+      if (titleA > titleB) {
+        return 1;
+      }
+      return 0;
+    });
+  
+    let top20movie = sortedTitles.slice (0,20)
+    
+    return top20movie;
+  }
+  
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(movies) {
+  let yearOrdered = [...movies].sort((a, b) => {
+    if (a.year !== b.year) {
+      return a.year - b.year;
+    } else {
+      // Si los años son iguales, comparar por título (alfabéticamente)
+      let titleA = a.title.toLowerCase();
+      let titleB = b.title.toLowerCase();
 
+      if (titleA < titleB) {
+        return -1;
+      }
+      if (titleA > titleB) {
+        return 1;
+      }
+      return 0;
+    }
+  });
+
+  return yearOrdered;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+function moviesAverageByCategory(movies, genre) {
 
+  let categoryArray = movies.filter(movie => movie.genre.includes(genre));
+
+  if (categoryArray.length === 0) {
+    return 0; 
+  }
+
+  let totalScore = categoryArray.reduce((total, movie) => total + movie.score, 0);
+  let averageScore = totalScore / categoryArray.length;
+
+  averageScore = Math.round(averageScore * 100) / 100;
+
+  return averageScore;
 }
+
+
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
+function hoursToMinutes(movies) {
+  let timeMovies = movies.map(movie => {
+    let durationParts = movie.duration.split(' '); 
+    let hours = parseInt(durationParts[0]); 
+    let minutes = 0;
 
+    if (durationParts.length > 1) {
+      minutes = parseInt(durationParts[1]); 
+    }
+
+    let totalMinutes = hours * 60 + minutes; 
+
+    return {
+      ...movie, 
+      duration: totalMinutes 
+    };
+  });
+
+  return timeMovies;
 }
+
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
+function bestFilmOfYear(movies, year) {
+  let moviesOfYear = movies.filter(movie => movie.year === year);
+
+  let bestMovie = moviesOfYear.reduce((best, current) => {
+    return best.score > current.score ? best : current;
+  });
+
+  return [bestMovie];;
 }
+
+
 
 
 
